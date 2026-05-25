a71fbf6cee2dd0e288960d28b851814972c4d84b
Diego ramon <126127427+Diegoramonn@users.noreply.github.com>
2026-05-25 13:17:40 -0300

    atu

--- Modal-Popup/modal-popup.js
+++ Modal-Popup/modal-popup.js
@@ -1,16 +1,31 @@
 const openModal =
- document.getElementById("open-modal")
+ document.getElementById("open-modal");
 
  const modal =
- document.getElementById("modal")
+ document.getElementById("modal");
 
  const closeModal =
- document.getElementById("close-modal")
+ document.getElementById("close-modal");
+
+ const cancelBtn = 
+ document.getElementById("cancel-btn");
+
 
  openModal.addEventListener("click", function(){
     modal.style.display = "flex"
- })
+ });
 
- closeModal.addEventListener("click", function(){
-    modal.style.display = "none"
- })
+closeModal.addEventListener("click", fecharModal)
+
+cancelBtn.addEventListener("click", fecharModal)
+
+function fecharModal(){
+   modal.style.display = "none "
+}
+
+window.addEventListener("click", function(event){
+   if(event.target === modal){
+      fecharModal()
+   }
+})
+
